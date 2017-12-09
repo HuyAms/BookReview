4b8bd6543d71b2f313dedf44f06878a0c3273a6c
HuyTrinh <dinhhuyams@gmail.com>
2017-12-09 15:37:02 +0200

    get bookdetail and comment

--- src/main/webapp/js/gototop.js
+++ src/main/webapp/js/gototop.js
@@ -8,7 +8,7 @@ window.onresize = () => {
 }
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = () => {scrollFunction()};
 
 function scrollFunction() {
   if (!mq.matches) {
--- target/BookReview-1.0-SNAPSHOT/js/mainpage.js
+++ target/BookReview-1.0-SNAPSHOT/js/mainpage.js
@@ -1,16 +1,36 @@
 /* --------------------------------------------------
   Handle book category navigation
 -----------------------------------------------------*/
-document.querySelector('#tabNews').addEventListener("click", () => {handleNavigation('news')});
-document.querySelector('#tabAll').addEventListener("click", () => {handleNavigation('all')});
-document.querySelector('#tabFiction').addEventListener("click", () => {handleNavigation('fiction')});
-document.querySelector('#tabNovel').addEventListener("click", () => {handleNavigation('novel')});
-document.querySelector('#tabTravel').addEventListener("click", () => {handleNavigation('travel')});
-document.querySelector('#tabGuide').addEventListener("click",() => {handleNavigation('guide')});
-document.querySelector('#tabHorror').addEventListener("click", () => {handleNavigation('horror')});
-document.querySelector('#tabRomance').addEventListener("click", () => {handleNavigation('romance')});
-document.querySelector('#tabScience').addEventListener("click", () => {handleNavigation('science')});
-document.querySelector('#tabOthers').addEventListener("click",() => {handleNavigation('others')});
+document.querySelector('#tabNews').addEventListener("click", () => {
+  handleNavigation('news')
+});
+document.querySelector('#tabAll').addEventListener("click", () => {
+  handleNavigation('all')
+});
+document.querySelector('#tabFiction').addEventListener("click", () => {
+  handleNavigation('fiction')
+});
+document.querySelector('#tabNovel').addEventListener("click", () => {
+  handleNavigation('novel')
+});
+document.querySelector('#tabTravel').addEventListener("click", () => {
+  handleNavigation('travel')
+});
+document.querySelector('#tabGuide').addEventListener("click", () => {
+  handleNavigation('guide')
+});
+document.querySelector('#tabHorror').addEventListener("click", () => {
+  handleNavigation('horror')
+});
+document.querySelector('#tabRomance').addEventListener("click", () => {
+  handleNavigation('romance')
+});
+document.querySelector('#tabScience').addEventListener("click", () => {
+  handleNavigation('science')
+});
+document.querySelector('#tabOthers').addEventListener("click", () => {
+  handleNavigation('others')
+});
 
 const handleNavigation = function(category) {
   hightLightTab(category);
@@ -32,7 +52,7 @@ const handleNavigation = function(category) {
 
 const hightLightTab = function(tab) {
   const listTab = document.querySelectorAll('.bookNav ul li');
-    listTab.forEach((tab, index) => {
+  listTab.forEach((tab, index) => {
     tab.classList.remove('tabActive');
   });
 
@@ -50,7 +70,7 @@ const loadBook = (category) => {
   let url = "";
   switch (category) {
     case 'all':
-       url = endPointUrl + `webresources/posts`;
+      url = endPointUrl + `webresources/posts`;
       break;
     default:
       url = endPointUrl + `webresources/posts/categories?filters=${category}`;
@@ -59,9 +79,7 @@ const loadBook = (category) => {
   fetch(url, {
     method: 'GET',
     headers: headers
-  })
-  .then(json)
-  .then((data) => {
+  }).then(json).then((data) => {
     if (data.hasOwnProperty('error')) {
       alert(data.error);
     } else {
@@ -77,8 +95,7 @@ const loadBook = (category) => {
           const bookId = book.post.postid;
           const bookAuthor = book.post.bookAuthor;
 
-          listBookHTML +=
-          `
+          listBookHTML += `
           <section class="book">
             <img src="${imgPath}" alt="${bookTitle}" class="bookImg bookModalTrigger" bookId="${bookId}"/>
             <p>${bookTitle}</p>
@@ -95,14 +112,15 @@ const loadBook = (category) => {
         let bookTrigger = document.querySelectorAll('.bookModalTrigger');
         bookTrigger.forEach((item, index) => {
           item.addEventListener('click', (evt) => {
-              bookModal.style.display = "block";
-              let bookId = evt.target.getAttribute('bookId');
-              // getBookDetail(bookId);
-              console.log('bookId: ' + bookId);
+            bookModal.style.display = "block";
+            let bookId = evt.target.getAttribute('bookId');
+            getBookDetail(bookId);
+            getComment(bookId);
+            getRating(bookId)
           })
         })
       }
-  }
+    }
   }).catch((error) => {
     console.log('error: ' + error);
   });
@@ -110,7 +128,101 @@ const loadBook = (category) => {
 
 //====================Book Detail=========================
 //[GET] Book Detail
+const getBookDetail = (bookId) => {
+  const url = endPointUrl + `webresources/posts/${bookId}`;
+  console.log('getBookDetail: ' + bookId);
+  fetch(url, {
+    method: 'GET',
+    headers: headers
+  }).then(json).then((data) => {
+    if (data.hasOwnProperty('error')) {
+      alert(data.error);
+    } else {
+      const author = data.bookAuthor;
+      const title = data.bookTitle;
+      const imgUrl = data.path;
+      const review = data.review;
+      const view = data.view;
+      const user = data.userUid.username;
+      const timestamp = data.timestamp;
+      console.log(data);
 
+      //show book reivew
+      document.querySelector('#modalHeader').innerHTML = title;
+      document.querySelector('#bookCover').innerHTML = `<img src="${imgUrl}" alt="gmat">`;
+      document.querySelector('#bookInfo').innerHTML = `<p>Author: ${author}<br> Publish year: 2017</p>`;
+      document.querySelector('#viewsCounts').innerHTML = view;
+      document.querySelector('#reviewAuthor').innerHTML = user;
+      document.querySelector('#reviewContent').innerHTML = review;
+
+      let t = new Date(timestamp);
+      document.querySelector('#reviewTime').innerHTML = `${t.getDate()}/${t.getMonth()}/${t.getFullYear()} ${t.getHours()}:${t.getMinutes()}`;
+    }
+  }).catch((error) => {
+    console.log('error: ' + error);
+  });
+}
+
+//[GET] Comments
+const getComment = (bookId) => {
+  const url = endPointUrl + `webresources/comments/posts/${bookId}`;
+
+  fetch(url, {
+    method: 'GET',
+    headers: headers
+  }).then(json).then((data) => {
+    if (data.hasOwnProperty('error')) {
+      alert(data.error);
+    } else {
+      let listComment = '';
+      //clear comment list
+      document.querySelector('#commentList').innerHTML = '';
+
+      if (data.length === 0) { //No comment
+        document.querySelector('#commentList').innerHTML = `<p>There are no comments yet Be the first to comment.</p>`;
+      } else {
+        document.querySelector('#commentsCounts').innerHTML = data.length;
+        data.forEach((comment) => {
+          let username = comment.userUid.username;
+          let content = comment.content;
+          let timestamp = comment.timestamp;
+          let t = new Date(timestamp);
+          listComment += `
+          <div class="wholeComment">
+            <h3 class="commentUser">${username}</h3>
+            <p class="commentTime">${t.getDate()}/${t.getMonth()}/${t.getFullYear()} ${t.getHours()}:${t.getMinutes()}</p>
+            <pre class="commentContent">${content}</pre>
+          </div>
+          `
+        });
+
+        document.querySelector('#commentList').innerHTML = listComment;
+      }
+
+    }
+  }).catch((error) => {
+    console.log('error: ' + error);
+  });
+}
+
+//[GET] Book Rating
+const getRating = (bookId) => {
+  const url = endPointUrl + `webresources/ratings/posts/${bookId}`;
+
+  fetch(url, {
+    method: 'GET',
+    headers: headers
+  }).then(json).then((data) => {
+    if (data.hasOwnProperty('error')) {
+      alert(data.error);
+    } else {
+      let rating = data.rating;
+      document.querySelector('#likesCounts').innerHTML = rating;
+    }
+  }).catch((error) => {
+    console.log('error: ' + error);
+  });
+}
 
 //====================Logout=========================
 document.querySelector('#buttonLogout').addEventListener('click', (evt) => {
